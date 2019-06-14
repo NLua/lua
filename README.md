5adf0238f84a5dff540d3bdeeb3fead5c49a0544
Vinicius Jarina <viniciusjarina@gmail.com>
2019-06-13 21:33:46 -0400

    Update with Lua 5.4 rc2

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-This is Lua 5.4.0 (alpha), released on 29 May 2019.
+This is Lua 5.4.0 (alpha), released on 06 Jun 2019.
 
 [![Build Status](https://secure.travis-ci.org/NLua/lua.svg?branch=lua54-rc1)](https://travis-ci.org/NLua/lua)
 
--- src/loslib.c
+++ src/loslib.c
@@ -135,15 +135,11 @@ static time_t l_checktime (lua_State *L, int arg) {
 /* }================================================================== */
 
 
-#if defined(LUA_USE_APPLE) && !defined(LUA_USE_MACOSX)
-#define lua_system(cmd) ((int)-1)
-#else
-#define lua_system(cmd) system(cmd)
-#endif
+
 
 static int os_execute (lua_State *L) {
   const char *cmd = luaL_optstring(L, 1, NULL);
-  int stat = lua_system(cmd);
+  int stat = system(cmd);
   if (cmd != NULL)
     return luaL_execresult(L, stat);
   else {
