1d4df6f5ff45ab16216de00249cc8711177e497f
Vinicius Jarina <viniciusjarina@gmail.com>
2020-04-22 21:03:04 -0700

    Fix Joining an upvalue with itself can cause a use-after-free crash https://www.lua.org/bugs.html#5.3.5-4

--- src/lapi.c
+++ src/lapi.c
@@ -1404,6 +1404,8 @@ LUA_API void lua_upvaluejoin (lua_State *L, int fidx1, int n1,
   LClosure *f1;
   UpVal **up1 = getupvalref(L, fidx1, n1, &f1);
   UpVal **up2 = getupvalref(L, fidx2, n2, NULL);
+  if (*up1 == *up2)
+    return;
   *up1 = *up2;
   luaC_objbarrier(L, f1, *up1);
 }
